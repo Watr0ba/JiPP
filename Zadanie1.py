f2aaa70bb820dfdefd13571059dfaacb2fe4d499
Dejam <Dejamsonn@gmail.com>
2026-01-09 19:17:50 +0100

    dodano Zadanie1

---
+++ Zadanie1.py
@@ -0,0 +1,21 @@
+#Najpierw utwórz zmienn¹ o nazwie x i przypisz jej wartoœæ 10
+
+x = 10
+
+#Nastêpnie utwórz drug¹ zmienn¹ y i przypisz jej wartoœæ 55
+
+y = 55
+
+#Utwórz zmienn¹ z i przypisz jej wartoœæ x + y
+
+z = x + y
+
+Zast¹p x wartoœci¹ z
+
+x = z
+
+#Wydrukuj wartoœæ x za pomoc¹ funkcji Pythona print()
+
+print(x)
+
+
